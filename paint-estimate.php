0bd692a0826f335b928ec9ee5c35995aae326c8e
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-03-04 12:59:02 -0500

    initial commit

---
+++ paint-estimate.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--	Author: 
+		Date:	
+		File:	paint-estimate.php
+		Purpose: OOP Exercise
+-->
+
+<html>
+<head>
+	<title>OOP Exercise</title>
+	<link rel ="stylesheet" type="text/css" href="sample.css"  />
+</head>
+
+<body>
+	<h1>Paint Estimate</h1>
+<?php
+
+	// your code here
+
+	$height = $_POST["height"];
+	$length = $_POST["length"];
+	$width = $_POST["width"];
+	
+	
+	
+	print("The total area is $totalArea square feet.");
+?>
+</body>
+</html>
---
+++ software-order.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--	Author: 
+		Date:	
+		File:	software-order.php
+		Purpose: OOP Exercise
+-->
+
+<html>
+<head>
+	<title>OOP Exercise</title>
+	<link rel ="stylesheet" type="text/css" href="sample.css"  />
+</head>
+
+<body>
+	<h1>Software Order</h1>
+<?php
+
+	// your code here
+
+	include_once("inc-order-object.php");
+
+	$cost = $_POST["cost"];
+	$items = $_POST["items"];
+
+	$order = new Order();
+	
+	$order->setItemCost($cost);
+	$order->setNumItems($items);
+
+	$subTotal = $order->getSubTotal();
+	$tax = $order->getSalesTax();
+	$shippingHandling = $order->getShippingHandling();
+	$total = $order->getTotal();
+
+	// you can change the variables in the table if you need to use different names
+
+	// right justify values
+	print("	<table>
+			<tr><td>Sub-Total:</td><td>$".number_format($subTotal, 2)."</td></tr>
+			<tr><td>Tax:</td><td>$".number_format($tax, 2)."</td></tr>
+			<tr><td>Shipping and Handling:</td><td>$".number_format($shippingHandling, 2)."</td></tr>
+			<tr><td>TOTAL:</td><td>$".number_format($total, 2)."</td></tr>
+			</table>");
+?>
+</body>
+</html>
