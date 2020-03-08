dfd4422028358e870515444b3346d7c425b8d14a
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-03-08 14:44:14 -0400

    Third Commit: Added an index file.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Asgn 07 Index</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+  </head>
+  <body>
+    <h1>Asgn 07 Work</h1>
+    <h2>Fixit Exercises</h2>
+    <p><a href="fixit1.php">Fixit 1</a></p>
+    <p><a href="fixit2.php">Fixit 2</a></p>
+    <p><a href="fixit3.php">Fixit 3</a></p>
+    <p><a href="fixit4.php">Fixit 4</a></p>
+    <p><a href="fixit5.php">Fixit 5</a></p>
+
+    <h2>Modify Exercises</h2>
+    <p><a href="modify1.html">Modify 1</a></p>
+    <p><a href="modify2.html">Modify 2</a></p>
+    <p><a href="modify3.html">Modify 3</a></p>
+
+    <h2>Code Completion Exercises</h2>
+    <p>Paint Estimate Exercise: <a href="paint-estimate.html">Paint Estimate</a></p>
+    <p>Software Order Exercise: <a href="software-order.html">Software Order</a></p>
+    <p>Game Character Exercise: <a href="game-character.php">Game Character</a></p>
+    <p>Cube Exercise: <a href="cube-test.php">Cube</a></p>
+    <p>Create a Class Exercise: <a href="get-distance.php">Create a Class 1</a></p>
+    <p>Create a Class Exercise: <a href="get-travel-cost.php">Create a Class 2</a></p>
+
+    <h1>Return Link</h1>
+    <p><a href="../index.php">Return to WEB 182 main page</a></p>
+  </body>
+</html>
